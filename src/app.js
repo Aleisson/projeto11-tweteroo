bfd94934c1faf920229fcc08c26559c6eda2c9a8
Aleisson <caioaleisson@gmail.com>
2022-08-28 19:18:33 -0300

    express --usejson

--- src/app.js
+++ src/app.js
@@ -1,15 +1,34 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
+
+// const user ={username:"", avatar:""};
+const users = []
 
 app.get("/", (req, res) => {
 
-    res.send("<h1>Hello World!!!</h1>")
+    res.send("<h1>Api Tweteroo</h1>")
+    
+
+})
+
+app.post("/sign-up", (req,res) =>{
+
+   
+    const user = req.body;
+
+    console.log(user.username);
+    console.log(user.avatar);
+    console.log(user);
+
+
+    res.send("OK");
 
 })
 
 
-
-
-app.listen(5000);
+app.listen(5000, () => console.log("Rodando Porta 5000"));
