f446757e075b5d9aa3ace1d90d320b2918e00945
Aleisson <caioaleisson@gmail.com>
2022-08-29 20:24:46 -0300

    deixando o projeto parecido com da aula

--- src/app.js
+++ src/app.js
@@ -9,30 +9,30 @@ app.use(express.json());
 // const user ={username:"", avatar:""};
 const users = [];
 const tweets = [];
-function filterList(index){
+// function filterList(index){
 
-    if(index > 9){
-        return false;
-    }
+//     if(index > 9){
+//         return false;
+//     }
 
-    return true;
+//     return true;
 
-}
+// }
+//modo que eu fiz
+// function addAvatar(tweet, users){
 
-function addAvatar(tweet, users){
+//     if(!tweet){
+//         return "sem avatar"
+//     }
 
-    if(!tweet){
-        return "sem avatar"
-    }
+//     const user = users.find(val => val.username === tweet.username);
+//     if(!user){
+//         return "sem avatar";
+//     }
 
-    const user = users.find(val => val.username === tweet.username);
-    if(!user){
-        return "sem avatar";
-    }
+//     return user.avatar;
 
-    return user.avatar;
-
-  };
+//   };
 
 
 app.get("/", (req, res) => {
@@ -57,10 +57,12 @@ app.post("/sign-up", (req,res) =>{
 
 
 app.post("/tweets", (req, res) =>{
-
-    const tweet = req.body;
-
-    tweets.push(tweet);
+    
+    const {username, tweet} = req.body;
+    const { avatar } = users.find(val => val.username === username);
+  
+    // console.log("aqui" + avatar);
+    tweets.push({username, tweet, avatar});
 
     // tweets.forEach(x => console.log(x));
 
@@ -70,11 +72,14 @@ app.post("/tweets", (req, res) =>{
 
 app.get("/tweets", (req, res)=>{
 
-    // const tweetsList = tweets.splice(-10).reverse();
-    const tweetsList = tweets.reverse().filter(((tweet,index) =>{
-       return filterList(index);
-    }));
-    tweetsList.forEach(x => x.avatar = addAvatar(x, users))
+    const tweetsList = tweets;
+    tweetsList.slice(-10).reverse();
+    
+   //modo como fiz
+    // const tweetsList = tweets.reverse().filter(((tweet,index) =>{
+    //    return filterList(index);
+    // }));
+    // tweetsList.forEach(x => x.avatar = addAvatar(x, users))
     // console.log(tweetsList);
     res.send(tweetsList);
 })
